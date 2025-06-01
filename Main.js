d91a8ad5c00cc5510a6f67773f3313ce13106087
Xcatgit0 <163300721+Xcatgit0@users.noreply.github.com>
2025-06-01 03:16:26 +0000

    ee

--- Main.js
+++ Main.js
@@ -1,3 +1,4 @@
 import { gcdByDivisionTable } from "./gcd";
 let resultE = document.getElementById('result');
 resultE.innerText = JSON.stringify(gcdByDivisionTable([120,50,60]));
+alert(gcdByDivisionTable([120,50,60]));
